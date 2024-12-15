643e9c92f3e84103eb5378c8a9feaa6bc5654876
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-12-15 15:41:34 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -28,6 +28,72 @@ installssr(){
 	     chmod +x ./install.sh
              ./install.sh
 	     }
+#安装新版bbr内核
+installnewbbr(){ kernel_version="5.9.6"
+  bit=$(uname -m)
+  rm -rf bbr
+  mkdir bbr && cd bbr || exit
+
+  if [[ "${release}" == "centos" ]]; then
+    if [[ ${version} == "7" ]]; then
+      if [[ ${bit} == "x86_64" ]]; then
+        echo -e "如果下载地址出错，可能当前正在更新，超过半天还是出错请反馈，大陆自行解决污染问题"
+        #github_ver=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | head -n 1 | awk -F '"' '{print $4}' | awk -F '[/]' '{print $8}' | awk -F '[_]' '{print $3}')
+        github_tag=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep 'Centos_Kernel' | grep '_latest_bbr_' | head -n 1 | awk -F '"' '{print $4}' | awk -F '[/]' '{print $8}')
+        github_ver=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'rpm' | grep 'headers' | awk -F '"' '{print $4}' | awk -F '[/]' '{print $9}' | awk -F '[-]' '{print $3}')
+        echo -e "获取的版本号为:${github_ver}"
+        kernel_version=$github_ver
+        detele_kernel_head
+        headurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'rpm' | grep 'headers' | awk -F '"' '{print $4}')
+        imgurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'rpm' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
+        #headurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/kernel-headers-${github_ver}-1.x86_64.rpm
+        #imgurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/kernel-${github_ver}-1.x86_64.rpm
+
+        headurl=$(check_cn $headurl)
+	imgurl=$(check_cn $imgurl)
+      echo -e "正在检查headers下载连接...."
+      checkurl $headurl
+      echo -e "正在检查内核下载连接...."
+      checkurl $imgurl
+      wget -N -O linux-headers-d10.deb $headurl
+      wget -N -O linux-image-d10.deb $imgurl
+      dpkg -i linux-image-d10.deb
+      dpkg -i linux-headers-d10.deb
+    elif [[ ${bit} == "aarch64" ]]; then
+      echo -e "如果下载地址出错，可能当前正在更新，超过半天还是出错请反馈，大陆自行解决污染问题"
+      github_tag=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep 'Debian_Kernel' | grep '_arm64_' | grep '_bbr_' | head -n 1 | awk -F '"' '{print $4}' | awk -F '[/]' '{print $8}')
+      github_ver=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}' | awk -F '[/]' '{print $9}' | awk -F '[-]' '{print $3}' | awk -F '[_]' '{print $1}')
+      echo -e "获取的版本号为:${github_ver}"
+      kernel_version=$github_ver
+      detele_kernel_head
+      headurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}')
+      imgurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
+      #headurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/linux-headers-${github_ver}_${github_ver}-1_amd64.deb
+      #imgurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/linux-image-${github_ver}_${github_ver}-1_amd64.deb
+
+      headurl=$(check_cn $headurl)
+      imgurl=$(check_cn $imgurl)
+      echo -e "正在检查headers下载连接...."
+      checkurl $headurl
+      echo -e "正在检查内核下载连接...."
+      checkurl $imgurl
+      wget -N -O linux-headers-d10.deb $headurl
+      wget -N -O linux-image-d10.deb $imgurl
+      dpkg -i linux-image-d10.deb
+      dpkg -i linux-headers-d10.deb
+    else
+      echo -e "${Error} 不支持x86_64及arm64/aarch64以外的系统 !" && exit 1
+    fi
+  fi
+
+  cd .. && rm -rf bbr
+
+  BBR_grub
+  echo -e "${Tip} 内核安装完毕，请参考上面的信息检查是否安装成功,默认从排第一的高版本内核启动"
+  check_kernel
+}
+	
+
 #安装bbr-v3
 installbbr-v3(){
              wget -N --no-check-certificate "https://raw.githubusercontent.com/ericyiu9819/bbr-v3/master/bbr-v3.sh"
@@ -473,22 +539,23 @@ echo && echo -e " TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ve
  ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
 ————————————内核管理————————————
  ${Green_font_prefix}1.${Font_color_suffix} 安装 BBR/BBR魔改版内核
- ${Green_font_prefix}2.${Font_color_suffix} 安装 BBRplus版内核 
- ${Green_font_prefix}3.${Font_color_suffix} 安装 Lotserver(锐速)内核
+ ${Green_font_prefix}2.${Font_color_suffix} 安装 BBR/BBR魔改版内核
+ ${Green_font_prefix}3.${Font_color_suffix} 安装 BBRplus版内核 
+ ${Green_font_prefix}4.${Font_color_suffix} 安装 Lotserver(锐速)内核
 ————————————加速管理————————————
- ${Green_font_prefix}4.${Font_color_suffix} 使用BBR加速
- ${Green_font_prefix}5.${Font_color_suffix} 使用BBR魔改版加速
- ${Green_font_prefix}6.${Font_color_suffix} 使用暴力BBR魔改版加速(不支持部分系统)
- ${Green_font_prefix}7.${Font_color_suffix} 使用BBRplus版加速
- ${Green_font_prefix}8.${Font_color_suffix} 使用Lotserver(锐速)加速
+ ${Green_font_prefix}5.${Font_color_suffix} 使用BBR加速
+ ${Green_font_prefix}6.${Font_color_suffix} 使用BBR魔改版加速
+ ${Green_font_prefix}7.${Font_color_suffix} 使用暴力BBR魔改版加速(不支持部分系统)
+ ${Green_font_prefix}8.${Font_color_suffix} 使用BBRplus版加速
+ ${Green_font_prefix}9.${Font_color_suffix} 使用Lotserver(锐速)加速
 ————————————杂项管理————————————
- ${Green_font_prefix}9.${Font_color_suffix} 卸载全部加速
- ${Green_font_prefix}10.${Font_color_suffix} 系统配置优化
- ${Green_font_prefix}11.${Font_color_suffix} 使用BBR+CAKE
- ${Green_font_prefix}12.${Font_color_suffix} 使用BBR+FQ_PIE
- ${Green_font_prefix}13.${Font_color_suffix} 安裝SSR
- ${Green_font_prefix}14.${Font_color_suffix} 安装bbr-v3
- ${Green_font_prefix}15.${Font_color_suffix} 退出腳本
+ ${Green_font_prefix}10.${Font_color_suffix} 卸载全部加速
+ ${Green_font_prefix}11.${Font_color_suffix} 系统配置优化
+ ${Green_font_prefix}12.${Font_color_suffix} 使用BBR+CAKE
+ ${Green_font_prefix}13.${Font_color_suffix} 使用BBR+FQ_PIE
+ ${Green_font_prefix}14.${Font_color_suffix} 安裝SSR
+ ${Green_font_prefix}15.${Font_color_suffix} 安装bbr-v3
+ ${Green_font_prefix}16.${Font_color_suffix} 退出腳本
  ————————————————————————————————" && echo
 
 	check_status
@@ -508,45 +575,48 @@ case "$num" in
 	check_sys_bbr
         ;;
 	2)
+	installnewbbr
+        ;;
+	3)
 	check_sys_bbrplus
 	;;
-	3)
+	4)
 	check_sys_Lotsever
 	;;
-	4)
+	5)
 	startbbr
 	;;
-	5)
+	6)
 	startbbrmod
 	;;
-	6)
+	7)
 	startbbrmod_nanqinlang
 	;;
-	7)
+	8)
 	startbbrplus
 	;;
-	8)
+	9)
 	startlotserver
 	;;
-	9)
+	10)
 	remove_all
 	;;
-	10)
+	11)
 	optimizing_system
 	;;
-	11)
+	12)
         startbbrcake
 	;;
-	12)
+	13)
         startbbrfqpie
         ;;
-	13)
+	14)
 	installssr
         ;;
-	15)
+	16)
         exit 1
 	;;
-        14)
+        15)
 	installbbr-v3
         ;;
         *)
