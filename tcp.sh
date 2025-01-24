2bb56bf081a304ac7741674fd47a8d539d8fc108
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2025-01-24 10:56:31 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -360,12 +360,10 @@ startbbrcake() {
 
 #启用BBR+fq_pie
 startbbrfqpie() {
-  remove_all
-   modprobe tcp_bbr
-           echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-           echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-           echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-           sysctl -p
+   remove_bbr_lotserver
+  echo "net.core.default_qdisc=fq_pie" >>/etc/sysctl.d/99-sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
+  sysctl --system
   echo -e "${Info}BBR+FQ_PIE修改成功，重启生效！"
 }
 
